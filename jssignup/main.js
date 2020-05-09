b2e54378b895d261c97d3a5ae941073c909cc147
Oabdalah23 <oabdalah23@gmail.com>
2020-05-09 12:35:44 +0300

    Minor Bug

--- jssignup/main.js
+++ jssignup/main.js
@@ -1,59 +1,4 @@
-(function ($) {
-    "use strict";
 
-
-    /*==================================================================
-    [ Validate ]*/
-    var input = $('.validate-input .input100');
-
-    $('.validate-form').on('submit',function(){
-        var check = true;
-
-        for(var i=0; i<input.length; i++) {
-            if(validate(input[i]) == false){
-                showValidate(input[i]);
-                check=false;
-            }
-        }
-
-        return check;
-    });
-
-
-    $('.validate-form .input100').each(function(){
-        $(this).focus(function(){
-           hideValidate(this);
-        });
-    });
-
-    function validate (input) {
-        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-                return false;
-            }
-        }
-        else {
-            if($(input).val().trim() == ''){
-                return false;
-            }
-        }
-    }
-
-    function showValidate(input) {
-        var thisAlert = $(input).parent();
-
-        $(thisAlert).addClass('alert-validate');
-    }
-
-    function hideValidate(input) {
-        var thisAlert = $(input).parent();
-
-        $(thisAlert).removeClass('alert-validate');
-    }
-    
-    
-
-})(jQuery);
 
 function signupchange () {
     deletetext();
@@ -186,6 +131,11 @@ function logout(){
     firebase.auth().signOut();
 }
 
+function back()
+{
+    setTimeout(redirect,1)
+}
+
 function redirect(){
     location.replace("https://KidKalc.com")
 }
@@ -194,4 +144,6 @@ function deletetext()
 {
     document.getElementById('error').innerHTML = '';
 }
+
+
   
