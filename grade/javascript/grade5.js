53139e902846fa9b2fd6d72ff91f6b1570078bdf
Oabdalah23 <oabdalah23@gmail.com>
2020-05-14 13:47:51 +0300

    Bugs

--- grade/javascript/grade5.js
+++ grade/javascript/grade5.js
@@ -36,8 +36,8 @@ if (JSON.parse(localStorage.getItem("savedscore5")) > 0) {
   score = 0;
 }
 
-if (JSON.parse(localStorage.getItem("savedcoin5")) > 0) {
-  coins = JSON.parse(localStorage.getItem("savedcoin5"));
+if (JSON.parse(localStorage.getItem("savedcoin")) > 0) {
+  coins = JSON.parse(localStorage.getItem("savedcoin"));
 } else {
   coins = 0;
 }
@@ -244,7 +244,7 @@ if (operation == "+") {
 
 function game() {
   coins = coins - 1;
-  localStorage.setItem("savedcoin5", JSON.stringify(coins));
+  localStorage.setItem("savedcoin", JSON.stringify(coins));
   window.location.href = "/game";
   document.getElementById("game").style.display = "none";
 }
@@ -260,7 +260,7 @@ function newquestion() {
   } else {
     document.getElementById("game").style.display = "none";
   }
-  localStorage.setItem("savedcoin5", JSON.stringify(coins));
+  localStorage.setItem("savedcoin", JSON.stringify(coins));
   document.getElementById("textbox").style.color = "white";
   document.getElementById("textbox").style.textShadow = "0.3vw 0.3vw #0095ff";
   document.getElementById("textbox").readOnly = false;
