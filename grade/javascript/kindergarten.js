423d0ba656200b8a39a9d32eaf3689c7d88ba6aa
Oabdalah23 <oabdalah23@gmail.com>
2020-06-01 15:30:26 +0300

    fix

--- grade/javascript/kindergarten.js
+++ grade/javascript/kindergarten.js
@@ -11,7 +11,7 @@ var config = {
 firebase.initializeApp(config);
 firebase.analytics();
 
-var myFBref = new Firebase ("https://kidkalc.firebaseio.com/");
+var myFBref = new Firebase("https://kidkalc.firebaseio.com/");
 
 $(document).ready(function () {
   var isshow = localStorage.getItem("isshow1");
